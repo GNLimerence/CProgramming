d174bab5c6e54cf79e4afee4951b54e1f8b97d5f
GNLimerence <phamhoanghainam203@gmail.com>
2023-11-03 01:41:45 +0700

    minifix

--- Binary.cpp
+++ Binary.cpp
@@ -18,6 +18,15 @@ void TRY (int k){
 		else TRY(k + 1);
 	}
 }
+/*
+Gỉai thích hàm TRY: ví dụ khi n = 3; xuất phát tại k = 1
+trong lần đầu tiên của vòng lặp x[1] = 0 gọi đệ quy với TRY(2), trong TRY(2)
+gọi vòng lặp x[2] = 0 sau đó gọi đệ quy với TRY(3), trong TRY(3) gọi vòng lặp x[3] = 0, 
+tại đây vì k == n nên sẽ in ra kết quả 000. Sau đó sẽ tiếp tục vòng lặp và gán x[k] == 1
+sau đó k == n nên sẽ in ra kết quả 001. Lúc này hàm hàm TRY đã được hoàn thành nên TRY(3)
+sẽ được đưa ra khỏi stack và lấy ra lời gọi đệ quy trước đó là TRY(2) và tiếp tục tại vị 
+trí được gọi đệ quy trước đó là TRY(3) nên lúc này sẽ tiếp tục vòng lặp và gán x[2] == 1. Keep going
+*/
 int main()
 {
 	cin >> n;
